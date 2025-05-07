37b8dcac0b689bb9c5e79317772c5027218b2591
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-05-07 14:05:37 +0530

    javaprogram

---
+++ Employee.java
@@ -0,0 +1,23 @@
+package dev;
+public class Employee {
+    String name;
+    int id;
+    double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayInfo() { // Corrected method name
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("xyz", 101, 50000.0);
+        emp1.displayInfo(); // Updated to match corrected method name
+    }
+}
